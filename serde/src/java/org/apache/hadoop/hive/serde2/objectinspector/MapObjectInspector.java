c3943a0b35b54660ab68f494ebba2f870a04adbe
Dhruba Borthakur <dhruba@apache.org>
2008-09-18 00:09:17 +0000

    HADOOP-4138. Refactor the Hive SerDe library to better structure the interfaces to the serializer and de-serializer. (Zheng Shao via dhruba)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/core/trunk/src/contrib/hive@696525 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7baa55c6da6e3f2baf2ebd26e6d7ebc2f7cd440e

---
+++ serde/src/java/org/apache/hadoop/hive/serde2/objectinspector/MapObjectInspector.java
@@ -0,0 +1,85 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one  * or more contributor license agreements.  See the NOTICE file  * distributed with this work for additional information  * regarding copyright ownership.  The ASF licenses this file  * to you under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance  * with the License.  You may obtain a copy of the License at  *  *     http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hive
+operator|.
+name|serde2
+operator|.
+name|objectinspector
+package|;
+end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Map
+import|;
+end_import
+
+begin_interface
+specifier|public
+interface|interface
+name|MapObjectInspector
+extends|extends
+name|ObjectInspector
+block|{
+comment|// ** Methods that does not need a data object **
+comment|// Map Type
+specifier|public
+name|ObjectInspector
+name|getMapKeyObjectInspector
+parameter_list|()
+function_decl|;
+specifier|public
+name|ObjectInspector
+name|getMapValueObjectInspector
+parameter_list|()
+function_decl|;
+comment|// ** Methods that need a data object **
+comment|// In this function, key has to be of the same structure as the Map expects.
+comment|// Most cases key will be primitive type, so it's OK.
+comment|// In rare cases that key is not primitive, the user is responsible for defining
+comment|// the hashCode() and equals() methods of the key class.
+specifier|public
+name|Object
+name|getMapValueElement
+parameter_list|(
+name|Object
+name|data
+parameter_list|,
+name|Object
+name|key
+parameter_list|)
+function_decl|;
+comment|/** returns null for data = null.    */
+specifier|public
+name|Map
+argument_list|<
+name|?
+argument_list|,
+name|?
+argument_list|>
+name|getMap
+parameter_list|(
+name|Object
+name|data
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
