4e638ced17277fb98b3022f45ba206b80c07f2a2
Zheng Shao <zshao@apache.org>
2009-08-19 19:07:31 +0000

    HIVE-756. Performance improvement for RCFile and ColumnarSerDe. (Ning Zhang via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@805934 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 678043f996f4d6a13016e1e462485f491850c347

--- ql/src/java/org/apache/hadoop/hive/ql/io/HiveFileFormatUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/HiveFileFormatUtils.java
@@ -1137,6 +1137,39 @@ operator|.
 name|length
 argument_list|)
 decl_stmt|;
+if|if
+condition|(
+name|list
+operator|.
+name|length
+operator|==
+literal|1
+operator|&&
+name|list
+index|[
+literal|0
+index|]
+operator|==
+literal|""
+condition|)
+block|{
+comment|// this is the case when conf value is specified by with a empty string ""
+comment|// the is to select all columns by default
+name|result
+operator|.
+name|add
+argument_list|(
+operator|new
+name|Integer
+argument_list|(
+operator|-
+literal|1
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
 for|for
 control|(
 name|int
@@ -1169,6 +1202,7 @@ index|]
 argument_list|)
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 return|return
 name|result
