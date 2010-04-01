d89189ec5ee77df97ce0b05409ffff6ea23a6d51
Zheng Shao <zshao@apache.org>
2010-04-01 22:26:00 +0000

    HIVE-1253. Fix Date_sub and Date_add in case of daylight saving. (Bryan Talbot via zshao)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/hive/trunk@930114 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0227b4bfdd2b886ae8f2ed63cd7a490bcb49523d

--- ql/src/java/org/apache/hadoop/hive/ql/udf/UDFDateSub.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/UDFDateSub.java
@@ -61,16 +61,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|TimeZone
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -188,14 +178,7 @@ init|=
 name|Calendar
 operator|.
 name|getInstance
-argument_list|(
-name|TimeZone
-operator|.
-name|getTimeZone
-argument_list|(
-literal|"UTC"
-argument_list|)
-argument_list|)
+argument_list|()
 decl_stmt|;
 specifier|private
 name|Text
