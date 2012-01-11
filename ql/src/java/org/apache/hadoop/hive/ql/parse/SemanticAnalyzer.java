110c102cf73b878c12615c0701313df0c91e89c2
Ashutosh Chauhan <hashutosh@apache.org>
2012-01-11 15:27:30 +0000

    HIVE-2705: SemanticAnalyzer twice swallows an exception it shouldn't (jghoman via hashutosh)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1230091 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ab5ccee2548949211b4c0184ee1f6dddd72e2597

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -25221,6 +25221,8 @@ name|e
 operator|.
 name|getMessage
 argument_list|()
+argument_list|,
+name|e
 argument_list|)
 throw|;
 block|}
@@ -26290,6 +26292,8 @@ name|e
 operator|.
 name|getMessage
 argument_list|()
+argument_list|,
+name|e
 argument_list|)
 throw|;
 block|}
