64aae2b2bb579cd12c7be457ad1de089c1d51593
Ashutosh Chauhan <hashutosh@apache.org>
2013-10-16 15:32:45 +0000

    HIVE-5546 : A change in ORCInputFormat made by HIVE4113 was reverted by HIVE5391 (Yin Huai via Ashutosh Chauhan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1532800 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 4041d2692f5fb5a19b01ad5da19ab3d4f9015480

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcInputFormat.java
@@ -1308,9 +1308,12 @@ name|Configuration
 name|conf
 parameter_list|)
 block|{
-name|String
-name|includedStr
-init|=
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"included column ids = "
+operator|+
 name|conf
 operator|.
 name|get
@@ -1319,14 +1322,6 @@ name|ColumnProjectionUtils
 operator|.
 name|READ_COLUMN_IDS_CONF_STR
 argument_list|)
-decl_stmt|;
-name|LOG
-operator|.
-name|info
-argument_list|(
-literal|"included column ids = "
-operator|+
-name|includedStr
 argument_list|)
 expr_stmt|;
 if|if
@@ -1337,20 +1332,6 @@ name|isReadAllColumns
 argument_list|(
 name|conf
 argument_list|)
-operator|||
-name|includedStr
-operator|==
-literal|null
-operator|||
-name|includedStr
-operator|.
-name|trim
-argument_list|()
-operator|.
-name|length
-argument_list|()
-operator|==
-literal|0
 condition|)
 block|{
 return|return
