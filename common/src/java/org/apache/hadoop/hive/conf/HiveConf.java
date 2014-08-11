a72bfea5e7504e7e881ec51dba77bf24a20ecb7b
Navis Ryu <navis@apache.org>
2014-08-11 01:16:30 +0000

    HIVE-7592 : List Jars or Files are not supported by Beeline (Navis reviewed by Szehon Ho)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1617189 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 270c2f9fdacfc09e0b3509c4ad16f7d0d5402a5d

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -6129,7 +6129,7 @@ name|HIVE_SECURITY_COMMAND_WHITELIST
 argument_list|(
 literal|"hive.security.command.whitelist"
 argument_list|,
-literal|"set,reset,dfs,add,delete,compile"
+literal|"set,reset,dfs,add,list,delete,compile"
 argument_list|,
 literal|"Comma separated list of non-SQL Hive commands users are authorized to execute"
 argument_list|)
