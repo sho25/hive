caf6f5b44072a703d3e3d5b8eed1910c3bf08e37
Sushanth Sowmyan <khorgath@apache.org>
2014-08-19 22:41:10 +0000

    HIVE-7068 : Integrate AccumuloStorageHandler (Josh Elser, reviewed by Navis, Nick Dimiduk & Sushanth Sowmyan)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1619005 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e78017f7f3e99808f47d4e91ad96965c25d376e9

---
+++ accumulo-handler/src/java/org/apache/hadoop/hive/accumulo/predicate/package-info.java
@@ -0,0 +1,23 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Predicate pushdown to Accumulo filter iterators.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|hadoop
+operator|.
+name|hive
+operator|.
+name|accumulo
+operator|.
+name|predicate
+package|;
+end_package
+
+end_unit
+
