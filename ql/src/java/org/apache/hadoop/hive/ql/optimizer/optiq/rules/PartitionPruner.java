0eeb2d0d087d2152ba5d489d1cdc9d16f029f1d3
Sergey Shelukhin <sershe@apache.org>
2014-10-30 20:11:12 +0000

    HIVE-8662 : CBO: tez_dml fails (Sergey Shelukhin, reviewed by Gunther Hagleitner)
    git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1635600 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 13ce856cc473acab8203ea21957d8197f0dc61c0

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/optiq/rules/PartitionPruner.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/optiq/rules/PartitionPruner.java
@@ -590,6 +590,13 @@ name|call
 operator|.
 name|getType
 argument_list|()
+argument_list|,
+name|call
+operator|.
+name|operands
+operator|.
+name|size
+argument_list|()
 argument_list|)
 decl_stmt|;
 if|if
