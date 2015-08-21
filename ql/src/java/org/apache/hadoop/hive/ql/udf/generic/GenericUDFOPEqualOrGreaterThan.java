3a9f1519943fffc6c9d9e295acca7ef615951fa0
Aihua Xu <aihuaxu@gmail.com>
2015-08-21 10:39:21 -0700

    HIVE-11375: Broken processing of queries containing NOT (x IS NOT NULL and x <> 0) (Aihua Xu, reviewed by Ashutosh Chauhan)
    Former-commit-id: a16267491978a35f4dd0478405b88a74725050a5

--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFOPEqualOrGreaterThan.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFOPEqualOrGreaterThan.java
@@ -799,6 +799,19 @@ name|GenericUDFOPEqualOrLessThan
 argument_list|()
 return|;
 block|}
+annotation|@
+name|Override
+specifier|public
+name|GenericUDF
+name|negative
+parameter_list|()
+block|{
+return|return
+operator|new
+name|GenericUDFOPLessThan
+argument_list|()
+return|;
+block|}
 block|}
 end_class
 
--- ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFOPOr.java
+++ ql/src/java/org/apache/hadoop/hive/ql/udf/generic/GenericUDFOPOr.java
@@ -611,6 +611,19 @@ name|toString
 argument_list|()
 return|;
 block|}
+annotation|@
+name|Override
+specifier|public
+name|GenericUDF
+name|negative
+parameter_list|()
+block|{
+return|return
+operator|new
+name|GenericUDFOPAnd
+argument_list|()
+return|;
+block|}
 block|}
 end_class
 
