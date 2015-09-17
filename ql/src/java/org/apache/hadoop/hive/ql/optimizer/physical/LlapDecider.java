050d990a3d8dd18c67dc1d7766a1051add009586
Sergey Shelukhin <sershe@apache.org>
2015-09-17 16:24:39 -0700

    HIVE-11851 : LLAP: Test failures - file.splitmetainfo file not found (Sergey Shelukhin, reviewed by Prasanth Jayachandran)
    Former-commit-id: 92ae3af4adf7564fb4fabcccfbb5d19b321a09fe

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/LlapDecider.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/physical/LlapDecider.java
@@ -1142,6 +1142,28 @@ comment|// first we check if we *can* run in llap. If we need to use
 comment|// user code to do so (script/udf) we don't.
 if|if
 condition|(
+name|work
+operator|instanceof
+name|MapWork
+operator|&&
+operator|(
+operator|(
+name|MapWork
+operator|)
+name|work
+operator|)
+operator|.
+name|isUseOneNullRowInputFormat
+argument_list|()
+condition|)
+block|{
+comment|// LLAP doesn't support file-based splits that this forces.
+return|return
+literal|false
+return|;
+block|}
+if|if
+condition|(
 operator|!
 name|evaluateOperators
 argument_list|(
