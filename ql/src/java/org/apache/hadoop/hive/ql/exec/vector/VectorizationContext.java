802320dae63e15db626f0c829e2ec168cb7e25d0
Matt McCline <mmccline@hortonworks.com>
2016-07-06 23:24:37 -0700

    Revert "HIVE-13924: (Vectorization) Error evaluating ((bool0 and (not bool1)) or (bool1 and (not bool0))) (Matt McCline, reviewed by Gopal Vijayaraghavan)"
    This reverts commit 9463231f78e787e712fc74440d7073da1b065461 [formerly c9d7ea5f5e9c45c9a49605b00aa532aac064725a].
    
    
    Former-commit-id: 223350894fe5aa653668e9f39e43218e514f2b24

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizationContext.java
@@ -7703,17 +7703,6 @@ throw|;
 block|}
 finally|finally
 block|{
-if|if
-condition|(
-name|childrenMode
-operator|!=
-name|VectorExpressionDescriptor
-operator|.
-name|Mode
-operator|.
-name|PROJECTION
-condition|)
-block|{
 for|for
 control|(
 name|VectorExpression
@@ -7732,7 +7721,6 @@ name|getOutputColumn
 argument_list|()
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 block|}
 block|}
