e95ec53bb5cbd5d4d73ada6851b69db6933fd01b
Rajesh Balamohan <rbalamohan at apache dot org>
2016-09-16 00:28:16 -0700

    HIVE-14764 : Enabling "hive.metastore.metrics.enabled" throws OOM in HiveMetastore (Rajesh Balamohan, reviewed by Vaibhav Gumashta)
    Former-commit-id: 7dd1d3694856a2fd55eba1355e81e5fa71477b0d

--- metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/HiveMetaStore.java
@@ -25753,7 +25753,9 @@ argument_list|()
 expr_stmt|;
 name|startFunction
 argument_list|(
-literal|"get_column_statistics_by_table: db="
+literal|"get_column_statistics_by_table"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -25816,7 +25818,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"get_column_statistics_by_table: "
+literal|"get_column_statistics_by_table"
 argument_list|,
 name|statsObj
 operator|!=
@@ -25869,7 +25871,9 @@ argument_list|()
 decl_stmt|;
 name|startFunction
 argument_list|(
-literal|"get_table_statistics_req: db="
+literal|"get_table_statistics_req"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -25980,7 +25984,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"get_table_statistics_req: "
+literal|"get_table_statistics_req"
 argument_list|,
 name|result
 operator|==
@@ -26056,7 +26060,9 @@ argument_list|)
 decl_stmt|;
 name|startFunction
 argument_list|(
-literal|"get_column_statistics_by_partition: db="
+literal|"get_column_statistics_by_partition"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -26157,7 +26163,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"get_column_statistics_by_partition: "
+literal|"get_column_statistics_by_partition"
 argument_list|,
 name|statsObj
 operator|!=
@@ -26213,7 +26219,9 @@ argument_list|()
 decl_stmt|;
 name|startFunction
 argument_list|(
-literal|"get_partitions_statistics_req: db="
+literal|"get_partitions_statistics_req"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -26398,7 +26406,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"get_partitions_statistics_req: "
+literal|"get_partitions_statistics_req"
 argument_list|,
 name|result
 operator|==
@@ -26519,6 +26527,23 @@ operator|.
 name|getStatsObj
 argument_list|()
 decl_stmt|;
+name|startFunction
+argument_list|(
+literal|"write_column_statistics"
+argument_list|,
+literal|":  db="
+operator|+
+name|dbName
+operator|+
+literal|" table="
+operator|+
+name|tableName
+operator|+
+literal|" column="
+operator|+
+name|colName
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|ColumnStatisticsObj
@@ -26541,21 +26566,6 @@ name|statsObj
 operator|.
 name|setColName
 argument_list|(
-name|colName
-argument_list|)
-expr_stmt|;
-name|startFunction
-argument_list|(
-literal|"write_column_statistics:  db="
-operator|+
-name|dbName
-operator|+
-literal|" table="
-operator|+
-name|tableName
-operator|+
-literal|" column="
-operator|+
 name|colName
 argument_list|)
 expr_stmt|;
@@ -26599,7 +26609,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"write_column_statistics: "
+literal|"write_column_statistics"
 argument_list|,
 name|ret
 operator|!=
@@ -26738,6 +26748,27 @@ operator|.
 name|getStatsObj
 argument_list|()
 decl_stmt|;
+name|startFunction
+argument_list|(
+literal|"write_partition_column_statistics"
+argument_list|,
+literal|":  db="
+operator|+
+name|dbName
+operator|+
+literal|" table="
+operator|+
+name|tableName
+operator|+
+literal|" part="
+operator|+
+name|partName
+operator|+
+literal|"column="
+operator|+
+name|colName
+argument_list|)
+expr_stmt|;
 for|for
 control|(
 name|ColumnStatisticsObj
@@ -26760,25 +26791,6 @@ name|statsObj
 operator|.
 name|setColName
 argument_list|(
-name|colName
-argument_list|)
-expr_stmt|;
-name|startFunction
-argument_list|(
-literal|"write_partition_column_statistics:  db="
-operator|+
-name|dbName
-operator|+
-literal|" table="
-operator|+
-name|tableName
-operator|+
-literal|" part="
-operator|+
-name|partName
-operator|+
-literal|"column="
-operator|+
 name|colName
 argument_list|)
 expr_stmt|;
@@ -26854,7 +26866,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"write_partition_column_statistics: "
+literal|"write_partition_column_statistics"
 argument_list|,
 name|ret
 operator|!=
@@ -26964,7 +26976,9 @@ argument_list|)
 decl_stmt|;
 name|startFunction
 argument_list|(
-literal|"delete_column_statistics_by_partition: db="
+literal|"delete_column_statistics_by_partition"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -27029,7 +27043,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"delete_column_statistics_by_partition: "
+literal|"delete_column_statistics_by_partition"
 argument_list|,
 name|ret
 operator|!=
@@ -27102,7 +27116,9 @@ expr_stmt|;
 block|}
 name|startFunction
 argument_list|(
-literal|"delete_column_statistics_by_table: db="
+literal|"delete_column_statistics_by_table"
+argument_list|,
+literal|": db="
 operator|+
 name|dbName
 operator|+
@@ -27141,7 +27157,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"delete_column_statistics_by_table: "
+literal|"delete_column_statistics_by_table"
 argument_list|,
 name|ret
 operator|!=
@@ -34415,7 +34431,9 @@ argument_list|()
 decl_stmt|;
 name|startFunction
 argument_list|(
-literal|"get_aggr_stats_for: db="
+literal|"get_aggr_stats_for"
+argument_list|,
+literal|": db="
 operator|+
 name|request
 operator|.
@@ -34551,7 +34569,7 @@ finally|finally
 block|{
 name|endFunction
 argument_list|(
-literal|"get_partitions_statistics_req: "
+literal|"get_aggr_stats_for"
 argument_list|,
 name|aggrStats
 operator|==
