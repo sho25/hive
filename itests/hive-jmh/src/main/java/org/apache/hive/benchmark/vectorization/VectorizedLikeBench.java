22732c8f24cf92eb5ae6a0d6a82305318a340954
Prasanth Jayachandran <prasanthj@apache.org>
2018-03-07 05:55:44 -0600

    HIVE-17163: Microbenchmark for vector op processing (Prasanth Jayachandran, reviewed by Matt McCline)
    Former-commit-id: 0cfd4feadd1b7dba206025cc20c5b8235231e8c5

--- itests/hive-jmh/src/main/java/org/apache/hive/benchmark/vectorization/VectorizedLikeBench.java
+++ itests/hive-jmh/src/main/java/org/apache/hive/benchmark/vectorization/VectorizedLikeBench.java
@@ -19,6 +19,18 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|nio
+operator|.
+name|charset
+operator|.
+name|StandardCharsets
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -124,18 +136,6 @@ operator|.
 name|options
 operator|.
 name|OptionsBuilder
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|nio
-operator|.
-name|charset
-operator|.
-name|StandardCharsets
 import|;
 end_import
 
