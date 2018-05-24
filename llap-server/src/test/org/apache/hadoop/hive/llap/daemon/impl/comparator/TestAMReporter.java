60635dccf80a77a76c18f355bbae3e513ec3d9a2
Jesus Camacho Rodriguez <jcamacho@apache.org>
2018-05-24 10:53:47 -0700

    HIVE-19698: TestAMReporter#testMultipleAM is flaky (Jesus Camacho Rodriguez, reviewed by Prasanth Jayachandran)
    Former-commit-id: 8d24b86de6d661817d00b1de0a91be382afa7c2f

--- llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/comparator/TestAMReporter.java
+++ llap-server/src/test/org/apache/hadoop/hive/llap/daemon/impl/comparator/TestAMReporter.java
@@ -165,6 +165,22 @@ name|org
 operator|.
 name|apache
 operator|.
+name|hive
+operator|.
+name|common
+operator|.
+name|util
+operator|.
+name|RetryTestRunner
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
 name|tez
 operator|.
 name|dag
@@ -182,6 +198,18 @@ operator|.
 name|junit
 operator|.
 name|Test
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|runner
+operator|.
+name|RunWith
 import|;
 end_import
 
@@ -346,6 +374,13 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|RunWith
+argument_list|(
+name|RetryTestRunner
+operator|.
+name|class
+argument_list|)
 specifier|public
 class|class
 name|TestAMReporter
