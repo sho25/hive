0adde7916ec8c817b1c186e9334f38881de02fd3
Ashutosh Chauhan <hashutosh@apache.org>
2018-06-13 15:06:40 -0500

    HIVE-19237: addendum to fix incorrect import
    Former-commit-id: 3ec5b0f0daaf3847839aff3d66815809e41e75ea

--- ql/src/java/org/apache/hadoop/hive/ql/hooks/NoOperatorReuseCheckerHook.java
+++ ql/src/java/org/apache/hadoop/hive/ql/hooks/NoOperatorReuseCheckerHook.java
@@ -81,16 +81,6 @@ end_import
 
 begin_import
 import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|hadoop
-operator|.
-name|hbase
-operator|.
-name|shaded
-operator|.
 name|com
 operator|.
 name|google
