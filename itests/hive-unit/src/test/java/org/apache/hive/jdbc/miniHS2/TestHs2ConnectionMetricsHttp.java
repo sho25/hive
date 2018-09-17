65249029a090f44eb1c577c17df60d4c75d3cd1a
Vaibhav Gumashta <vgumashta@hortonworks.com>
2018-09-17 10:48:24 -0700

    HIVE-20577: Disable org.apache.hive.jdbc.miniHS2.TestHs2ConnectionMetricsHttp.testOpenConnectionMetrics (Vaibhav Gumashta reviewed by Daniel Dai)
    Former-commit-id: 393b382af00013c72c074c79d71b261c3c9e3bd3

--- itests/hive-unit/src/test/java/org/apache/hive/jdbc/miniHS2/TestHs2ConnectionMetricsHttp.java
+++ itests/hive-unit/src/test/java/org/apache/hive/jdbc/miniHS2/TestHs2ConnectionMetricsHttp.java
@@ -309,6 +309,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -389,6 +399,11 @@ name|tearDown
 argument_list|()
 expr_stmt|;
 block|}
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Flaky test. Should be re-enabled in HIVE-20578"
+argument_list|)
 annotation|@
 name|Test
 specifier|public
