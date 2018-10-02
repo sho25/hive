fe3d204f3bbfa60629d5eadc21f176b08c53f4ef
Karen Coppage <karen.coppage@cloudera.com>
2018-10-02 12:18:52 +0200

    HIVE-20662: Disable TestMiniLlapLocalCliDriver.testCliDriver[load_dyn_part3] (Karen Coppage, reviewed by Zoltan Haindrich and Adam Szita)
    Former-commit-id: 499539fb62b4aa8908d517816f27a0e4997555a7

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -883,6 +883,12 @@ literal|"schema_evol_orc_vec_part_llap_io.q"
 argument_list|)
 expr_stmt|;
 comment|// Disabled in HIVE-19509
+name|excludeQuery
+argument_list|(
+literal|"load_dyn_part3.q"
+argument_list|)
+expr_stmt|;
+comment|// Disabled in HIVE-20662. Enable in HIVE-20663.
 name|setResultsDir
 argument_list|(
 literal|"ql/src/test/results/clientpositive/llap"
