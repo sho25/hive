6c8c286d980579d9de60171db35b720734844477
Zoltan Haindrich <kirk@rxd.hu>
2018-12-12 13:09:28 +0100

    HIVE-21022: Disable test: TestRemoteHiveMetaStoreZKBindHost
    Former-commit-id: 5f742cfef8a29e92f2547c5fc9f45094fbc1fa1f

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestRemoteHiveMetaStoreZKBindHost.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/TestRemoteHiveMetaStoreZKBindHost.java
@@ -79,6 +79,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Before
 import|;
 end_import
@@ -98,6 +108,11 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"HIVE-21022: disabled until fixed"
+argument_list|)
 annotation|@
 name|Category
 argument_list|(
