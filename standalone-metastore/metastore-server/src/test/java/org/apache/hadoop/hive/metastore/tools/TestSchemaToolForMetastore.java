a45f55e522ff044b67100b3d18d34c74446b890f
Vihang Karajgaonkar <vihangk1@apache.org>
2019-01-16 12:27:56 -0800

    Revert "HIVE-21077 : Database and Catalogs should have creation time (Vihang Karajgaonkar reviewed by Karthik Manamcheri and Bharath Krishna)"
    This reverts commit e0a9196861393f1a564b1ae0a70eb5a0282d6023 [formerly 4d8320d467d30e4e125a3304407415885670f9c7].
    
    
    Former-commit-id: 4d5d80609608521db3d12916f9900b88a94aff94

--- standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/tools/TestSchemaToolForMetastore.java
+++ standalone-metastore/metastore-server/src/test/java/org/apache/hadoop/hive/metastore/tools/TestSchemaToolForMetastore.java
@@ -732,21 +732,6 @@ argument_list|(
 name|isValid
 argument_list|)
 expr_stmt|;
-name|String
-name|time
-init|=
-name|String
-operator|.
-name|valueOf
-argument_list|(
-name|System
-operator|.
-name|currentTimeMillis
-argument_list|()
-operator|/
-literal|1000
-argument_list|)
-decl_stmt|;
 comment|// Test valid case
 name|String
 index|[]
@@ -756,19 +741,11 @@ operator|new
 name|String
 index|[]
 block|{
-literal|"insert into CTLGS values(99, 'test_cat_1', 'description', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into CTLGS values(99, 'test_cat_1', 'description', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb');"
 block|,
 literal|"insert into SEQUENCE_TABLE values('org.apache.hadoop.hive.metastore.model.MDatabase', 100);"
 block|,
-literal|"insert into DBS values(99, 'test db1', 'hdfs:///tmp', 'db1', 'test', 'test', 'test_cat_1', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(99, 'test db1', 'hdfs:///tmp', 'db1', 'test', 'test', 'test_cat_1');"
 block|}
 decl_stmt|;
 name|File
@@ -818,11 +795,7 @@ literal|"delete from DBS;"
 block|,
 literal|"insert into SEQUENCE_TABLE values('org.apache.hadoop.hive.metastore.model.MDatabase', 100);"
 block|,
-literal|"insert into DBS values(102, 'test db1', 'hdfs:///tmp', 'db1', 'test', 'test', 'test_cat_1', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(102, 'test db1', 'hdfs:///tmp', 'db1', 'test', 'test', 'test_cat_1');"
 block|}
 expr_stmt|;
 name|scriptFile
@@ -1977,21 +1950,6 @@ argument_list|(
 name|isValid
 argument_list|)
 expr_stmt|;
-name|String
-name|time
-init|=
-name|String
-operator|.
-name|valueOf
-argument_list|(
-name|System
-operator|.
-name|currentTimeMillis
-argument_list|()
-operator|/
-literal|1000
-argument_list|)
-decl_stmt|;
 comment|// Test valid case
 name|String
 index|[]
@@ -2001,23 +1959,11 @@ operator|new
 name|String
 index|[]
 block|{
-literal|"insert into CTLGS values(3, 'test_cat_2', 'description', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into CTLGS values(3, 'test_cat_2', 'description', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb');"
 block|,
-literal|"insert into DBS values(2, 'my db', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(2, 'my db', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'test_cat_2');"
 block|,
-literal|"insert into DBS values(7, 'db with bad port', 'hdfs://myhost.com:8020/', 'haDB', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(7, 'db with bad port', 'hdfs://myhost.com:8020/', 'haDB', 'public', 'role', 'test_cat_2');"
 block|,
 literal|"insert into SDS(SD_ID,CD_ID,INPUT_FORMAT,IS_COMPRESSED,IS_STOREDASSUBDIRECTORIES,LOCATION,NUM_BUCKETS,OUTPUT_FORMAT,SERDE_ID) values (1,null,'org.apache.hadoop.mapred.TextInputFormat','N','N','hdfs://myhost.com:8020/user/hive/warehouse/mydb',-1,'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',null);"
 block|,
@@ -2123,29 +2069,13 @@ literal|"delete from SDS;"
 block|,
 literal|"delete from DBS;"
 block|,
-literal|"insert into DBS values(2, 'my db', '/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(2, 'my db', '/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'test_cat_2');"
 block|,
-literal|"insert into DBS values(4, 'my db2', 'hdfs://myhost.com:8020', '', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(4, 'my db2', 'hdfs://myhost.com:8020', '', 'public', 'role', 'test_cat_2');"
 block|,
-literal|"insert into DBS values(6, 'db with bad port', 'hdfs://myhost.com:8020:', 'zDB', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(6, 'db with bad port', 'hdfs://myhost.com:8020:', 'zDB', 'public', 'role', 'test_cat_2');"
 block|,
-literal|"insert into DBS values(7, 'db with bad port', 'hdfs://mynameservice.com/', 'haDB', 'public', 'role', 'test_cat_2', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(7, 'db with bad port', 'hdfs://mynameservice.com/', 'haDB', 'public', 'role', 'test_cat_2');"
 block|,
 literal|"insert into SDS(SD_ID,CD_ID,INPUT_FORMAT,IS_COMPRESSED,IS_STOREDASSUBDIRECTORIES,LOCATION,NUM_BUCKETS,OUTPUT_FORMAT,SERDE_ID) values (1,null,'org.apache.hadoop.mapred.TextInputFormat','N','N','hdfs://yourhost.com:8020/user/hive/warehouse/mydb',-1,'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',null);"
 block|,
@@ -2609,21 +2539,6 @@ throws|throws
 name|IOException
 block|{
 name|String
-name|time
-init|=
-name|String
-operator|.
-name|valueOf
-argument_list|(
-name|System
-operator|.
-name|currentTimeMillis
-argument_list|()
-operator|/
-literal|1000
-argument_list|)
-decl_stmt|;
-name|String
 index|[]
 name|scripts
 init|=
@@ -2631,17 +2546,9 @@ operator|new
 name|String
 index|[]
 block|{
-literal|"insert into CTLGS values (1, 'mycat', 'my description', 'hdfs://myhost.com:8020/user/hive/warehouse', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into CTLGS values (1, 'mycat', 'my description', 'hdfs://myhost.com:8020/user/hive/warehouse');"
 block|,
-literal|"insert into DBS values(2, 'my db', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'mycat', "
-operator|+
-name|time
-operator|+
-literal|");"
+literal|"insert into DBS values(2, 'my db', 'hdfs://myhost.com:8020/user/hive/warehouse/mydb', 'mydb', 'public', 'role', 'mycat');"
 block|,
 literal|"insert into SDS(SD_ID,CD_ID,INPUT_FORMAT,IS_COMPRESSED,IS_STOREDASSUBDIRECTORIES,LOCATION,NUM_BUCKETS,OUTPUT_FORMAT,SERDE_ID) values (1,null,'org.apache.hadoop.mapred.TextInputFormat','N','N','hdfs://myhost.com:8020/user/hive/warehouse/mydb',-1,'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',null);"
 block|,
