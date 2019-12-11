e8328230ee2b02dd3a280d57fb79d69fbbf5155b
Oliver Draese <odraese@cloudera.com>
2019-12-10 18:18:26 -0800

    HIVE-22616: Disable PreCommit test org.apache.hadoop.hive.ql.TestMTQueries.testMTQueries1 (Oliver Draese via Jesus Camacho Rodriguez)
    Former-commit-id: 8b18c3c70d5ec2a4549d9ab7e610d92ee8506d55

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestMTQueries.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/TestMTQueries.java
@@ -33,6 +33,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -98,6 +108,11 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Disabled due to intermittent failures (HIVE-22616)"
+argument_list|)
 annotation|@
 name|Test
 specifier|public
