099988f1299c73279d57afa48346ab31e8ff68b8
Zoltan Haindrich <kirk@rxd.hu>
2020-01-07 16:08:58 +0000

    HIVE-20972: Enable TestMiniLlapLocalCliDriver cbo_limit and cbo_rp_limit (Zoltan Haindrich reviewed by Peter Vary)
    Signed-off-by: Zoltan Haindrich <kirk@rxd.hu>
    
    Former-commit-id: cf5a208a452bd63848d876a926d6019cb70fa7e9

--- itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
+++ itests/util/src/main/java/org/apache/hadoop/hive/cli/control/CliConfigs.java
@@ -1088,22 +1088,10 @@ expr_stmt|;
 comment|// Disabled in HIVE-20662. Enable in HIVE-20663.
 name|excludeQuery
 argument_list|(
-literal|"cbo_limit.q"
-argument_list|)
-expr_stmt|;
-comment|//Disabled in HIVE-20860. Enable in HIVE-20972
-name|excludeQuery
-argument_list|(
 literal|"rfc5424_parser_file_pruning.q"
 argument_list|)
 expr_stmt|;
 comment|// Disabled in HIVE-21427
-name|excludeQuery
-argument_list|(
-literal|"cbo_rp_limit.q"
-argument_list|)
-expr_stmt|;
-comment|// Disabled in HIVE-21657
 name|setResultsDir
 argument_list|(
 literal|"ql/src/test/results/clientpositive/llap"
